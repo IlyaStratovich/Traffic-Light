719d565459541d563de5b0fb9c29897179622bd5
Ilya Stratovich <ilyastratovitch@yandex.by>
2023-01-31 14:14:23 +0300

    Final version.

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Color {
+    case red
+    case yellow
+    case green
+}
 
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    
+    @IBOutlet weak var selectorButton: UIButton!
+    
+    private var color = Color.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        selectorButton.layer.cornerRadius = 10
+        
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
+        
+        redColorView.alpha = lightOff
+        yellowColorView.alpha = lightOff
+        greenColorView.alpha = lightOff
+        
     }
-
-
+    
+    @IBAction func selectorButtonDidTap() {
+        
+        if selectorButton.currentTitle != "NEXT" {
+            selectorButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch color {
+        case .red:
+            greenColorView.alpha = lightOff
+            redColorView.alpha = lightOn
+            color = .yellow
+        case .yellow:
+            redColorView.alpha = lightOff
+            yellowColorView.alpha = lightOn
+            color = .green
+        case .green:
+            yellowColorView.alpha = lightOff
+            greenColorView.alpha = lightOn
+            color = .red
+        }
+    }
+    
 }
 
